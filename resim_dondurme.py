506f10ed420eb63be096e07290a95adbe35c15c9
Melik Mirza Çelik <mecelik1905@gmail.com>
2026-07-16 09:36:23 +0300

    resmi dondurme

---
+++ resim_dondurme.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("C:/Users/Master/Desktop/OpenCV/resim_dondurme/7.jpg",0)
+row,col = img.shape
+
+M = cv2.getRotationMatrix2D((col/2,row/2),180,3)
+
+dst = cv2.warpAffine(img,M,(col,row))
+
+cv2.imshow("dst",dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
