f05b4eaffdb0e92b33778ba471cfa3bfa704c878
Melik Mirza Çelik <mecelik1905@gmail.com>
2026-07-23 11:01:12 +0300

    Add files via upload

---
+++ convexhull.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("C:/Users/Master/Desktop/OpenCV/convexhull/map.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.blur(gray, (3, 3))
+ret, thresh = cv2.threshold(blur, 40, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("original", img)
+cv2.imshow("gray", gray)
+cv2.imshow("blur", blur)
+cv2.imshow("threshold", thresh)
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
